1e647bf6c7fbd3287a113607e71b7b650c3106e3
Ritesh Gupta <guptarg3a@gmail.com>
2026-08-15 18:27:57 +0000

    Add Chapter4.py with logic for boiling status, milk presence, and serving conditions

---
+++ 01-datatypes/Chapter4.py
@@ -0,0 +1,19 @@
+is_boiling = True
+stri_count = 5
+total_actions = stri_count + is_boiling # upcasting
+print(f"Total actions: {total_actions}")
+
+milk_present = 0 # no milk
+print(f"Is there milk? {bool(milk_present)}")
+
+water_hot = True
+tea_added = True
+
+can_server = water_hot and tea_added
+print(f"Can serve chai? {can_server}")
+
+
+
+
+# Upcasting - Automatic conversion to a compatible wider type: int + float → float
+# Downcasting - Explicit conversion to another/narrower type: float → int
