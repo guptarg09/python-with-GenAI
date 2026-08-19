58b289eb25acb88942c04ba6936767103ef32ac9
Ritesh Gupta <guptarg3a@gmail.com>
2026-08-19 18:08:03 +0000

    Add 04_tea_menu.py for displaying tea menu options

---
+++ 03-loops/04_tea_menu.py
@@ -0,0 +1,7 @@
+menu = ["Green", "Lemon", "Spiced", "Mint"]
+
+for idx, item in enumerate(menu, start=1):
+    print(f"{idx} : {item} chai")
+
+
+# enumerate() allows you to loop over a collection while getting both the index and the value.
